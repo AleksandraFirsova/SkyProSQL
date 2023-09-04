85a96fd9102034f1638d8e2245830881b2177866
alexandrafirsova <alsfirsova@gmail.com>
2023-09-04 18:46:07 +0300

    first commit

---
+++ SkyProSQL/src/main/java/com/example/skyprosql/SkyProSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyprosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProSqlApplication.class, args);
+    }
+
+}
---
+++ SkyProSQL/src/test/java/com/example/skyprosql/SkyProSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
